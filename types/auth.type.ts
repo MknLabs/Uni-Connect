c00a4e1da393214af07997f2d0704b9f5db6d223
Mkn0021 <nayeem21122002@gmail.com>
2025-12-23 13:12:18 +0600

    chore: breaking changes  - migrate from node to bun  - upgrade to next-js 16.1.1 along with react  - remove unneccessary files for cleanup

--- types/auth.type.ts
+++ types/auth.type.ts
@@ -1,16 +1,29 @@
 import { z } from 'zod';
-import { CreateUserSchema } from "./user.type";
 
-export const SignupFormSchema = CreateUserSchema.body.extend({
-    confirmPassword: z.string()
-}).refine((data) => data.password === data.confirmPassword, {
-    message: 'Passwords do not match',
-    path: ['confirmPassword']
+export const UserSchema = z.object({
+    id: z.string(),
+    name: z.string()
+        .min(2, "Name must be at least 2 characters")
+        .max(50, "Name too long")
+        .regex(/^[a-zA-Z0-9\s]+$/, "Name can only contain letters, numbers, and spaces"),
+    email: z.email("Invalid email format").nonempty("Email is required").toLowerCase().trim(),
+    password: z.string().min(6, "Password must be at least 6 characters").optional(),
 });
 
-export const LoginFormSchema = CreateUserSchema.body.pick({
-    email: true,
-    password: true
+export const SignupFormSchema = z.object({
+    name: UserSchema.shape.name,
+    email: UserSchema.shape.email,
+    password: UserSchema.shape.password.unwrap(),
+    confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+});
+
+
+export const LoginFormSchema = z.object({
+    email: UserSchema.shape.email,
+    password: UserSchema.shape.password.unwrap(),
 });
 
 export type SignupForm = z.infer<typeof SignupFormSchema>;
